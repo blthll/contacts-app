7dc3d5ff3db8d27880a5a07f0dde4f54e4a99d0b
blthll <bolat.halil@std.izu.edu.tr>
2022-04-24 20:21:41 +0300

    Filter and last commit

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,12 +1,21 @@
 import  {useState,useEffect}  from "react";
 import List from "./List";
 import Form from "./Form";
+import "./styles.css";
 function Contacts(){
-    const[contacts,setContacts] =useState([]);
+    const[contacts,setContacts] =useState([
+        {fullname:'Halil',
+         phone_number:'1234'},
+         {fullname:'Enes',
+         phone_number:'1234'},
+         {fullname:'Batu',
+         phone_number:'1234'},
+    ]);
     useEffect(()=>{
         console.log(contacts);
     },[contacts]);
-    return (<div>
+    return (<div id="container">
+        <h1>Contacts</h1>
         <List contacts={contacts}/>
         <Form addContact={setContacts} contacts={contacts}/>
     </div>);
